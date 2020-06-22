fe6d4678db08aaf877009370dccc4ef57747150f
Michael Tharpe <mtharpe@hashicorp.com>
2020-06-22 11:53:30 -0400

    Update to add instance

--- main.tf
+++ main.tf
@@ -4,8 +4,45 @@ provider "google" {
   region      = var.google_region
 }
 
-resource "google_compute_instance" "default" {
-  name         = "test"
+resource "google_compute_instance" "demo1" {
+  name         = "demo1"
+  machine_type = "n1-standard-1"
+  zone         = "us-central1-a"
+
+  tags = ["foo", "bar"]
+
+  boot_disk {
+    initialize_params {
+      image = "ubuntu-os-cloud/ubuntu-1804-bionic-v20200521"
+    }
+  }
+
+  // Local SSD disk
+  scratch_disk {
+    interface = "SCSI"
+  }
+
+  network_interface {
+    network = "default"
+
+    access_config {
+      // Ephemeral IP
+    }
+  }
+
+  metadata = {
+    foo = "bar"
+  }
+
+  metadata_startup_script = "sudo apt update && sudo apt upgrade -y --auto-remove"
+
+  service_account {
+    scopes = ["userinfo-email", "compute-ro", "storage-ro"]
+  }
+}
+
+resource "google_compute_instance" "demo2" {
+  name         = "demo2"
   machine_type = "n1-standard-1"
   zone         = "us-central1-a"
 
