3985dfdc61b925a361539240164ecec4b1297025
Mike Tharpe <4297677+mtharpe@users.noreply.github.com>
2020-08-21 21:42:16 -0400

    Update main.tf
    Update to instance name

--- main.tf
+++ main.tf
@@ -84,7 +84,7 @@ resource "google_sql_database" "database" {
 }
 
 resource "google_sql_database_instance" "instance" {
-  name   = "my-database-instance"
+  name   = "${var.google_project}-my-database-instance"
   region = "us-central1"
   settings {
     tier = "db-f1-micro"
