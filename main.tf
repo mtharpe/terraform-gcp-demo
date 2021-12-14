4a6a6688a1a52d9e55a3d94f72cb339c5cfffd24
Michael Tharpe <mtharpe@hashicorp.com>
2021-12-13 23:09:14 -0500

    updated for lint

--- main.tf
+++ main.tf
@@ -8,8 +8,8 @@ terraform {
 }
 
 provider "google" {
-  project     = var.gcp_project
-  region      = var.gcp_region
+  project = var.gcp_project
+  region  = var.gcp_region
 }
 
 resource "google_compute_instance" "demo1" {
