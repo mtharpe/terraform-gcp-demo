7b7aba9a14386481ea425cf8f45e5601230dce71
Srikanth Arunachalam <sriaruna@WKMZT61A5F24.global.publicisgroupe.net>
2023-02-17 14:48:10 +0000

    test 1.0.0

--- variables.tf
+++ variables.tf
@@ -1,16 +1,20 @@
 variable "gcp_instance_username" {
+  default = wheelinst1
   description = "GCP Instance Username"
 }
 
 variable "gcp_instance_password" {
+  default = "wheelinst1"
   description = "GCP Instance Password"
 }
 
 variable "gcp_project" {
+  default = "wheelinst1"
   description = "GCP Project Name"
 }
 
 variable "gcp_region" {
+  default = "wheelinst1"
   description = "GCP Project Region"
 }
 
