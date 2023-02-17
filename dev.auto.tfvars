5d9107369a569c6102ff5f9bd3c257f3e3888acc
Srikanth Arunachalam <sriaruna@WKMZT61A5F24.global.publicisgroupe.net>
2023-02-17 14:57:17 +0000

    test 1.0.1

---
+++ dev.auto.tfvars
@@ -0,0 +1,5 @@
+# terraform.tfvars
+gcp_region = "europe-west3"
+gcp_instance_username = "srikarundev"
+gcp_instance_password = "srikarundev"
+gcp_project = "mock-terratest-gcp-feb17"
--- variables.tf
+++ variables.tf
@@ -1,20 +1,16 @@
 variable "gcp_instance_username" {
-  default = wheelinst1
   description = "GCP Instance Username"
 }
 
 variable "gcp_instance_password" {
-  default = "wheelinst1"
   description = "GCP Instance Password"
 }
 
 variable "gcp_project" {
-  default = "wheelinst1"
   description = "GCP Project Name"
 }
 
 variable "gcp_region" {
-  default = "wheelinst1"
   description = "GCP Project Region"
 }
 
