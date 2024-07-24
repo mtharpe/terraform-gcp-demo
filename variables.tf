bb3cb4e6bb364f9f016f57e0889c4382460b7b9e
Michael Tharpe <mtharpe@gmail.com>
2024-07-24 18:20:54 -0400

    Update

--- variables.tf
+++ variables.tf
@@ -1,16 +1,20 @@
 variable "gcp_instance_username" {
+  type = string
   description = "GCP Instance Username"
 }
 
 variable "gcp_instance_password" {
+  type = string
   description = "GCP Instance Password"
 }
 
 variable "gcp_project" {
+  type = string
   description = "GCP Project Name"
 }
 
 variable "gcp_region" {
+  type = string
   description = "GCP Project Region"
 }
 
